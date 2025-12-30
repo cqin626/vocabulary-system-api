4a964178724a8a2bd0eacca82c3cbaaa67746b0d
cqin626 <qinhoh@gmail.com>
2025-12-30 12:06:22 +0800

    Set up api v1 router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import routerV1 from "./routes/routeV1.js";
 
 const app = express();
+
+app.use("/api/v1", routerV1);
 
 app.use((req, res) => {
   res.status(404).json({
