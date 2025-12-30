0654830bc49218281895e212c113f31adc353eb4
cqin626 <qinhoh@gmail.com>
2025-12-30 11:49:53 +0800

    Create a default route handler

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    error: {
+      message: "Endpoint not found",
+    },
+  });
+});
+
 export default app;
