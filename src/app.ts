92cd3837fd1acd190b35b196f15641e40e42ab00
cqin626 <qinhoh@gmail.com>
2025-12-30 10:18:06 +0800

    Separate between app and server

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
 import express from "express";
-import "dotenv/config"
 
 const app = express();
-const PORT = Number(process.env.PORT) || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} `);
-});
+export default app;
